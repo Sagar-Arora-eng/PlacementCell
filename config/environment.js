3d37f2ab0bb87129e77ff8a1a6bcabc9a77400ec
Sagar Arora <sagararor403@gmail.com>
2023-05-27 10:04:56 +0530

    project completed final commit

---
+++ config/environment.js
@@ -0,0 +1,11 @@
+const developement = {
+  name: "developement",
+  db_path: "./config/mongoose",
+  passport_path: "./config/passport-local-strategy",
+  customMware_path: "./config/flashMessage",
+  assets_path: "./assets",
+  secret_key: "#placementCell@careercamp$$Team&&&interface##Sagar^^Arora%%",
+  mongoose_path: "mongodb://0.0.0.0:27017/placementCell",
+  api_path: "https://remotive.com/api/remote-jobs",
+};
+module.exports = developement;
